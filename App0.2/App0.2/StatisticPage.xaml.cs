c29293ff987be2c546ff45ea4edf6ed24d5b219a
Диана Соколова <dosokolova@edu.hse.ru>
2021-03-20 19:10:12 +0500

    +Charts

--- App0.2/App0.2/StatisticPage.xaml.cs
+++ App0.2/App0.2/StatisticPage.xaml.cs
@@ -13,19 +13,31 @@ namespace App0._2
     
     public partial class StatisticPage : ContentPage
     {
-        List<string> Photos = VkParser.PhotoUploadUrl.Keys.ToList<string>();
+        public static List<string> Photos = VkParser.PhotoUploadUrl.Keys.ToList<string>();
         //string url = "https://sun9-34.userapi.com/impf/c853624/v853624041/83de7/4q6hzLRhHIQ.jpg?size=1080x1080&quality=96&sign=3d5bbbf91532c0d6b5dee499430b2caf&type=album";
         public StatisticPage()
         {
             InitializeComponent();
-            //ActivityIndicator loading = new ActivityIndicator() {IsRunning = true, IsVisible = true };
-
+            ActivityIndicator loading = new ActivityIndicator() {IsRunning = true, IsVisible = true };
+            StatisticButton.Clicked += StatisticButton_Clicked;
+            Photos = VkParser.PhotoUploadUrl.Keys.ToList<string>();
             AddImages();
-            //loading.IsRunning = false;
-            //loading.IsVisible = false;
+            loading.IsRunning = false;
+            loading.IsVisible = false;
 
         }
 
+        /// <summary>
+        /// Открытие страницы общей статистики.
+        /// </summary>
+        private void StatisticButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new GeneralStatisticPage());
+        }
+
+        /// <summary>
+        /// Добавление изображение в сетку.
+        /// </summary>
         private void AddImages()
         {
             for (int i = 0; i< Photos.Count / 3 + Photos.Count % 3 % 2; i++)
@@ -62,6 +74,7 @@ namespace App0._2
 
             }
         }
+
         /// <summary>
         ///  Обработка события нажатия на изображение - переход к статистики по фото.
         /// </summary>
